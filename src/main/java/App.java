7d894cb219d4c69a3e73ef9ee00253cce4c07500
Albert M <albert.mukhametzianov@gmail.com>
2022-06-02 21:33:37 +0500

    0 Release

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        // Bean equals bean2 ?
+        String result1 = (bean1 == bean2) ? "Переменные (bean1 & bean2) ссылаются на один и тот же объект!" :
+                                            "Переменные (bean1 & bean2) ссылаются на разные объекты!";
+
+        // Create cat bean
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        String result2 = (cat1 == cat2) ? "Переменные (cat1 & cat2) ссылаются на один и тот же объект!" :
+                                          "Переменные (cat1 & cat2) ссылаются на разные объекты!";
+
+        System.out.println(result1);
+        System.out.println(result2);
+
     }
 }
